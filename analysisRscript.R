af6dfa1b44d17a22c06f555ff306a666e70abbd1
Stefan Fernandez <stefan.fernandez@gmail.com>
2013-11-14 21:20:17 -0500

    made progress in getting team average. graphs not really working how i want them

--- analysisRscript.R
+++ analysisRscript.R
@@ -8,25 +8,38 @@ all_games <- list("~/repos/nfl_modeling/1.csv", "~/repos/nfl_modeling/2.csv", "~
 all_games <- lapply(my.path, read.csv)
 
 #This selects the game from the game list and converts it into a data frame.
-#testdf <- as.data.frame(all_games[2])
+testdf <- as.data.frame(all_games[6])
 
-avg <- data.frame(1:15)
+avg <- data.frame(1:15,1:15)
+
+#colnames(avg[1]) <- "avg"
+colnames(avg)[1] <- "team"
+colnames(avg)[2] <- "yards"
+#avg$team <- as.factor(avg$team)
+
+
 #get average of team stats over the season
 for (k in 1:16){
   single_game <- as.data.frame(all_games[k])
   #makes new data frame and then adds subset to new data frame
   single_game[is.na(single_game)] <- 0
-  test <- single_game[single_game$team == "SF", ]
-  avg[k,] <- sum(test$passing_yds, test$rushing_yds)
+  single_game_subset <- single_game[single_game$team != "SF", ]
+  avg[k,1] <- as.character(single_game_subset$team[1])  
+  avg[k,2] <- sum(single_game_subset$passing_yds)#, test$rushing_yds)
 }
 
-ggplot(data = mydata, aes(x = fumbles_lost, y = team, color=team, shape="Cash Flow/Debt")) +
-  geom_point() +
-  ggtitle("Company Health")+
-  ylab("Health Ratios")+
-  xlab("Companies")+
-  geom_point(data = mydata, aes(x = team, y = fumbles_lost, color=team, shape="Profit/Value")) +
-  labs(color = "Company Still Active?", shape = "Metrics")
+#Graph boxplot
+p <- ggplot(data = avg, aes(x=team, y=yards), labels=avg$team) + geom_bar(stat="identity")
+p <- p + scale_x_discrete(breaks=team)
+p
+
+#boxplot(data=avg,x=avg$yards, main="passing yards allowed", xlab="SF", ylab="Passing Yards")
+
+#p <- ggplot(data = avg, ylim=c(0,500), aes(x=c(1:16), y=avg)) + geom_line() + geom_point()
+#plot(c(1:16),avg$yards, main="title", sub="subtitle", xlab="X-axis label", ylab="y-axix label",
+#     xlim=c(1, 16), ylim=c(0, 500))
+
+
 
 fumbles = data.frame(1:1520)
 for (k in 1:1520){
