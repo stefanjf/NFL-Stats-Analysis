9a3939361fbe3ff2e5ef8093cfb74329c67c613c
Stefan Fernandez <stefan.fernandez@gmail.com>
2013-11-14 22:12:48 -0500

    cool graph working with pass yards allowed vs average yards allowed by diff opponents

--- analysisRscript.R
+++ analysisRscript.R
@@ -1,6 +1,6 @@
 library(ggplot2)
 
-#mydata = read.csv("~/Fantasy Football/2012.csv", sep=",", header=T)   
+mydata = read.csv("~/Fantasy Football/2012.csv", sep=",", header=T)   
 #Wk1 = read.csv("~/repos/nfl_modeling/1.csv", sep=",", header=T) 
 
 #Quick way to load all csv files at once
@@ -14,14 +14,26 @@ all_games <- lapply(path, read.csv)
 #This selects the game from the game list and converts it into a data frame.
 testdf <- as.data.frame(all_games[6])
 
-avg <- data.frame(1:15,1:15,1:15)
 
-#colnames(avg[1]) <- "avg"
-colnames(avg)[1] <- "team"
-colnames(avg)[2] <- "pass_yards"
-colnames(avg)[3] <- "rush_yards"
 
-#avg$team <- as.factor(avg$team)
+
+#Yearly stats. Get rid of NAs
+mf <- data.frame(mydata)
+mf[is.na(mf)] <- 0
+#list of all team names
+teams <- c("ATL","BUF","CHI","CIN","CLE","BAL","DAL","DEN","DET","GB","TEN","IND","KC","OAK","STL","MIA","MIN","NE","NO","NYG","NYJ","PHI","PIT","SD","SF","SEA","ARI","TB","WAS","CAR","JAC","HOU")
+teamStats <- as.data.frame(teams)
+#Gets team stats for entire year
+for (k in 1:32){
+  test <- mf[mf$team == teams[k], ]
+  teamStats[k,2] <- sum(test$passing_yds)#, test$rushing_yds)
+}
+colnames(teamStats) <- c("team", "yards")
+teamStatsOrdered <- teamStats[order(teamStats$yards),]
+teamStatsOrdered<-data.frame(teamStatsOrdered, row.names=NULL)
+teamStatsOrdered[,3] <- c(1:32)
+
+avg <- data.frame(1:15,1:15,1:15,1:15)
 
 #get average of team stats over the season
 for (k in 1:16){
@@ -32,13 +44,33 @@ for (k in 1:16){
   avg[k,1] <- as.character(single_game_subset$team[1])  
   avg[k,2] <- sum(single_game_subset$passing_yds)#, test$rushing_yds)
   avg[k,3] <- sum(single_game_subset$rushing_yds)#, test$rushing_yds)
-  
+  #find average for each opponent and put it in column
+  season_total <- teamStats[teamStats$team == as.character(single_game_subset$team[1]), ][2]
+  avg[k,4] <- season_total/16
 }
+
+#colnames(avg[1]) <- "avg"
+colnames(avg)[1] <- "team"
+colnames(avg)[2] <- "pass_yards"
+colnames(avg)[3] <- "rush_yards"
+colnames(avg)[4] <- "opp"
 
 #THIS GRAPH WORKS!!!
 p <- ggplot(data = avg, aes(x=c(1:16), y=avg$pass_yards), labels=avg$team) + geom_line() + geom_point() + scale_x_discrete(labels=avg$team) + ylim(0,500) +
-geom_line(data = avg, aes(x=c(1:16), y=avg$rush_yards, colour="#000099"),show_guide = FALSE) + geom_point(data = avg, aes(x=c(1:16), y=avg$rush_yards, colour="#000099"),show_guide = FALSE)
+geom_line(data = avg, aes(x=c(1:16), y=avg$opp, colour="#000099"),show_guide = FALSE) + geom_point(data = avg, aes(x=c(1:16), y=avg$opp, colour="#000099"),show_guide = FALSE)
 p
+
+
+
+
+
+
+
+
+
+
+
+
 
 #boxplot(data=avg,x=avg$yards, main="passing yards allowed", xlab="SF", ylab="Passing Yards")
 
@@ -59,22 +91,7 @@ qplot(mydata$team, cumsum(mydata$fumbles_lost), data=mydata, xlim=c(1:10), y)
 
 #  test <- mf[mf$team == "SF", ]
 
-#makes new data frame and then adds subset to new data frame
-mf <- data.frame(mydata)
-mf[is.na(mf)] <- 0
 
-#list of all team names
-teams <- c("ATL","BUF","CHI","CIN","CLE","BAL","DAL","DEN","DET","GB","TEN","IND","KC","OAK","STL","MIA","MIN","NE","NO","NYG","NYJ","PHI","PIT","SD","SF","SEA","ARI","TB","WAS","CAR","JAC","HOU")
-teamStats <- as.data.frame(teams)
-
-for (k in 1:32){
-   test <- mf[mf$team == teams[k], ]
-   teamStats[k,2] <- sum(test$passing_yds, test$rushing_yds)
-}
-colnames(teamStats) <- c("team", "yards")
-teamStatsOrdered <- teamStats[order(teamStats$yards),]
-teamStatsOrdered<-data.frame(teamStatsOrdered, row.names=NULL)
-teamStatsOrdered[,3] <- c(1:32)
 
 p <- ggplot(data = teamStatsOrdered, aes(x=ordered, y=yards), labels=teamStatsOrdered$team) + geom_bar(stat="identity")
 p <- p + scale_x_discrete(limits=teamStatsOrdered$team)
